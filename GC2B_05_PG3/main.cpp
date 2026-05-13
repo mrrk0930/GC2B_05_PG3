e83b9937339dda2b9870141252a6c739acd2729d
gc2b_murosaki <k025g1008@g.neec.ac.jp>
2026-05-13 16:07:42 +0900

    関数ポインタ

--- GC2B_05_PG3/main.cpp
+++ GC2B_05_PG3/main.cpp
@@ -1,59 +1,104 @@
-#include<iostream>
-#include<Windows.h>
-#include<stdio.h>
-
-int add(int a, int b) 
-{
-
-	return a + b;
-
-}
-
-int sub(int a, int b)
-{
-
-	return a - b;
-
-}
+#include <iostream>
+#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 typedef void (*PFunc)(int*);
 
-void DispResult(int* s) 
+// 勝ち
+void Win(int* sum)
 {
-
-	printf("%d秒\n", *s);
-
+	printf("\nサイコロの目の和 : %d\n", *sum);
+	printf("結果 : 勝ち\n");
 }
 
-void setTimeout(PFunc p, int second) 
+// 負け
+void Lose(int* sum)
 {
+	printf("\nサイコロの目の和 : %d\n", *sum);
+	printf("結果 : 負け\n");
+}
 
+// 入力
+int InputPlayer()
+{
+	int num;
+
+	printf("丁(偶数)なら0を入力\n");
+	printf("半(奇数)なら1を入力\n");
+
+	scanf_s("%d", &num);
+
+	return num;
+}
+
+// サイコロランダム
+int RollDice()
+{
+	return rand() % 6 + 1;
+}
+
+// 判定
+PFunc Judge(int player, int sum)
+{
+	
+	if (player == 0)
+	{
+		if (sum % 2 == 0)
+		{
+			return Win;
+		}
+
+		return Lose;
+	}
+
+	
+	if (sum % 2 != 0)
+	{
+		return Win;
+	}
+
+	return Lose;
+}
+
+
+void setTimeout(PFunc p, int second, int* sum)
+{
+	printf("\nサイコロを振っています...\n");
+
+	
 	Sleep(second * 1000);
 
-	p(&second);
-
+	p(sum);
 }
 
 int main()
 {
 	SetConsoleOutputCP(65001);
 
-	int l = 100;
-	int h = 200;
+	srand((unsigned int)time(nullptr));
 
-	int (*calc)(int, int);
+	
+	int player = InputPlayer();
 
-	calc = add;
-	printf("%d\n", calc(l, h));
+	
+	int dice1 = RollDice();
+	int dice2 = RollDice();
 
-	calc = sub;
-	printf("%d\n", calc(l, h));
+	
+	int sum = dice1 + dice2;
 
-	printf("\nstart\n");
+	
+	PFunc resultFunc;
+	resultFunc = Judge(player, sum);
 
-	PFunc p;
-	p = DispResult;
-	setTimeout(p, 5);
+	
+	setTimeout(resultFunc, 3, &sum);
+
+
+	printf("\nサイコロの目1 : %d\n", dice1);
+	printf("サイコロの目2 : %d\n", dice2);
 
 	return 0;
 }
