40040a30521c726d327e0e97a782da8f4ef4779b
gc2b_murosaki <k025g1008@g.neec.ac.jp>
2026-05-27 15:13:01 +0900

    メンバ関数ポインタ

--- GC2B_05_PG3/main.cpp
+++ GC2B_05_PG3/main.cpp
@@ -1,119 +1,116 @@
 #include <iostream>
 #include <Windows.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-typedef void (*PFunc)(int*);
+using namespace std;
 
-//遅延
-void setTimeout(PFunc p, int second, int* sum)
+
+class Enemy
 {
+public:
+
+	// 状態
+	enum class Phase
+	{
+		Approach, // 接近
+		Shot,     // 射撃
+		Leave     // 離脱
+	};
+
+	// コンストラクタ
+	Enemy()
+	{
+		phase_ = Phase::Approach;
+		timer_ = 0;
+	}
+
+	// 更新
+	void Update()
+	{
+		
+		(this->*spFuncTable[static_cast<size_t>(phase_)])();
+	}
+
+private:
+
 	
-	printf("\nサイコロを振っています.....\n");
+	// 接近
+	
+	void Approach()
+	{
+		printf("敵が接近しています\n");
 
-	Sleep(second * 1000);
+		timer_++;
 
-	p(sum);
+		
+		if (timer_ >= 1)
+		{
+			timer_ = 0;
+			phase_ = Phase::Shot;
+		}
+	}
 
-}
+	
+	// 射撃
+	
+	void Shot()
+	{
+		printf("敵が射撃しています\n");
 
-int main(int argc, const char* argv[])
+		timer_++;
+
+		
+		if (timer_ >= 1)
+		{
+			timer_ = 0;
+			phase_ = Phase::Leave;
+		}
+	}
+
+	
+	// 離脱
+
+	void Leave()
+	{
+		printf("敵が離脱しています\n");
+
+		timer_++;
+	}
+
+private:
+
+	
+	Phase phase_;
+
+	
+	int timer_;
+
+	
+	static void (Enemy::* spFuncTable[])();
+};
+
+
+void (Enemy::* Enemy::spFuncTable[])() =
 {
-	
+	&Enemy::Approach, // 接近
+	&Enemy::Shot,     // 射撃
+	&Enemy::Leave     // 離脱
+};
+
+
+int main()
+{
 	SetConsoleOutputCP(65001);
 
-	srand((unsigned int)time(nullptr));
-
-	//勝ち
-	auto Win = [](int* sum)
-	{
-
-			printf("\nサイコロの目の和 : %d\n", *sum);
-			printf("結果 : 正解\n");
-
-	};
-
-	//負け
-	auto Lose = [](int* sum)
-	{
-			
-			printf("\nサイコロの目の和 : %d\n", *sum);
-			printf("結果 : 不正解\n");
-	
-	};
-
-	//入力
-	auto InputPlayer = []()
-	{
-			
-			int num;
-
-			printf("丁(偶数)なら0を入力\n");
-			printf("半(奇数)なら1を入力\n");
-
-			scanf_s("%d", &num);
-
-			return num;
-	
-	};
-
-	//サイコロ
-	auto RollDice = []()
-	{
-			
-			return rand() % 6 + 1;
-	
-	};
+	Enemy enemy;
 
 	
-	//入力
-	int player = InputPlayer();
+	for (int i = 0; i < 3; i++)
+	{
+		enemy.Update();
 
-
-	//サイコロ
-	int dice1 = RollDice();
-	int dice2 = RollDice();
-
-	//合計
-	int sum = dice1 + dice2;
-
-	//丁半判定
-	PFunc resultFunc;
-
-	//
-	auto Judge = [&](int sum)
-		{
-			// 丁
-			if (player == 0)
-			{
-				if (sum % 2 == 0)
-				{
-					return +Win;
-				}
-
-				return +Lose;
-			}
-
-			// 半
-			if (sum % 2 != 0)
-			{
-				return +Win;
-			}
-
-			return +Lose;
-		};
-
-	//
-	resultFunc = Judge(sum);
-
-	//遅延実行
-	setTimeout(resultFunc, 3, &sum);
-
-    //表示
-	printf("\nサイコロ1 : %d\n", dice1);
-	printf("サイコロ2 : %d\n", dice2);
+		Sleep(1000);
+	}
 
 	return 0;
-
 }
